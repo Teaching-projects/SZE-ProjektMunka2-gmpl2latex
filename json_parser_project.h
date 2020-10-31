9d78ea4665c3f9dbc6c6d6c5f9e3dcafa61ff0d7
Gabor Gonczi <gonczigabor98@gmail.com>
2020-10-31 22:20:35 +0100

    Documentation added and implementation is cleaned

--- json_parser_project.h
+++ json_parser_project.h
@@ -1,4 +1,3 @@
-#include <iostream>
 /*c++ libraries include*/
 #include <iostream>
 #include <map>
@@ -9,36 +8,27 @@
 /*Rapidjson includes*/
 #include "filereadstream.h"
 #include "document.h"
-#include "writer.h"
 #include "filewritestream.h"
 #include "prettywriter.h"
-//Header contains declaration
-using namespace rapidjson;
-using namespace std;
 
 typedef std::vector<std::string> STLCONTAINER;
 
-void ObjectParser(rapidjson::Value::ConstObject o, std::map<std::string, std::string>&eredmenyMap) {
+void objectParser(rapidjson::Value::ConstObject o, std::map<std::string, std::string>&eredmenyMap) {
 
-	for (rapidjson::Value::ConstMemberIterator it = o.MemberBegin(); it != o.MemberEnd(); ++it) {
-
-		if (it->value.IsObject()) {
-			ObjectParser(it->value.GetObject(), eredmenyMap);
+	for (rapidjson::Value::ConstMemberIterator it = o.MemberBegin(); it != o.MemberEnd(); ++it)
+	{
+		if (it->value.IsObject())
+		{
+			objectParser(it->value.GetObject(), eredmenyMap);
 		}
 		else
 		{
-			//printf("Fuggvenyen belul irodik ki %d szinten\n",melyseg);
-			//printf("\nNev: %s, Ertek: %s\n", it->name.GetString(), it->value.GetString());
-
-			//pair<std::string, std::string>p = std::make_pair<std::string, std::string>(a, b);
 			eredmenyMap.emplace(std::make_pair<std::string, std::string>(it->name.GetString(), it->value.GetString()));
-			//std::cout << "A meyseg valtozatlanul a fuggvenyen belul" << melyseg << std::endl;
 		}
 	}
-
-
 }
-std::map<std::string, std::string>JSON_TO_MAP(const char* fname, const char*mod) {
+std::map<std::string, std::string>jsonToMap(const char* fname, const char*mod)
+{
 	std::map<std::string, std::string> eredmenyMap;
 	FILE*fp = fopen(fname, mod);
 	char buffer[65000];
@@ -51,19 +41,12 @@ std::map<std::string, std::string>JSON_TO_MAP(const char* fname, const char*mod)
 	}
 	for (rapidjson::Value::ConstMemberIterator it = object.MemberBegin(); it != object.MemberEnd(); ++it)
 	{
-		if (it->value.IsObject()) {
-
-			ObjectParser(it->value.GetObject(), eredmenyMap);
-			//eredmenyMap.emplace(std::make_pair<std::string, std::string>(it->name.GetString(), it->value.GetString()));
-
+		if (it->value.IsObject())
+		{
+			objectParser(it->value.GetObject(), eredmenyMap);
 		}
-		/*if (it->value.IsString()) {
-							eredmenyMap.emplace(std::make_pair<std::string, std::string>(it->name.GetString(), it->value.GetString()));
-					}*/
 		else
 		{
-			//printf("Fuggvenyen kivul irodik ki\n");
-			//printf("Nev: %s Ertek: %s\n", it->name.GetString(), it->value.GetString());
 			eredmenyMap.emplace(std::make_pair<std::string, std::string>(it->name.GetString(), it->value.GetString()));
 		}
 
@@ -71,7 +54,7 @@ std::map<std::string, std::string>JSON_TO_MAP(const char* fname, const char*mod)
 	return eredmenyMap;
 
 }
-Value ObjectCreator(STLCONTAINER next,Document&output) {
+Value objectCreator(STLCONTAINER next, Document&output) {
 	Value ret(kObjectType);
 	Value name(kStringType);
 	Value val(kObjectType);
@@ -84,7 +67,7 @@ Value ObjectCreator(STLCONTAINER next,Document&output) {
 	}
 	return ret;
 }
-Value setofSetsCreator(STLCONTAINER next, Document&output) {
+Value setOfSetsCreator(STLCONTAINER next, Document&output) {
 	Value ret(kObjectType);
 	Value name(kStringType);
 	Value val(kObjectType);
@@ -93,12 +76,12 @@ Value setofSetsCreator(STLCONTAINER next, Document&output) {
 		Value tmpSet(kObjectType);
 		std::string tmp = *it;
 		std::string tmpVal = tmp;
-		std::string tmpName = tmp+"_name";
+		std::string tmpName = tmp + "_name";
 		tmpVal = toupper(tmp[0]);
 		val.SetString(tmpVal.c_str(), static_cast<SizeType>(tmpVal.length()), allocator);
 		name.SetString(tmpName.c_str(), allocator);
 		tmpSet.AddMember(name, val, allocator);
-		tmpName = tmp+"_default-index";
+		tmpName = tmp + "_default-index";
 		tmpVal = tolower(tmp[0]);
 		val.SetString(tmpVal.c_str(), static_cast<SizeType>(tmpVal.length()), allocator);
 		name.SetString(tmpName.c_str(), allocator);
@@ -108,9 +91,9 @@ Value setofSetsCreator(STLCONTAINER next, Document&output) {
 	return ret;
 }
 
-Document createJSON(int componentsCount...) {
+Document createJson(int componentsCount...) {
 
-	std::vector<std::string> tmpVector;
+	STLCONTAINER tmpVector;
 	Document output;
 	output.SetObject();
 	va_list components;
@@ -124,59 +107,21 @@ Document createJSON(int componentsCount...) {
 		for (int i = 0; i < componentsCount; i++)
 		{
 			tmpVector = va_arg(components, STLCONTAINER);
-			if (i == 0) { output.AddMember("Sets", setofSetsCreator(tmpVector, output), output.GetAllocator()); }
-			else if (i == 1) { output.AddMember("Parameters", ObjectCreator(tmpVector, output), output.GetAllocator()); }
-			else if (i == 2) { output.AddMember("Variables", ObjectCreator(tmpVector, output), output.GetAllocator()); }
-			/*{
-			case 0:output.AddMember("Sets", setofSetsCreator(tmpVector, output), output.GetAllocator());
-				break;
-			case 1:output.AddMember("Parameters", ObjectCreator(tmpVector,output), output.GetAllocator());
-				break;
-			case 2:output.AddMember("Variables", ObjectCreator(tmpVector, output), output.GetAllocator());
-				break;
-			}*/
-
+			if (i == 0) { output.AddMember("Sets", setOfSetsCreator(tmpVector, output), output.GetAllocator()); }
+			else if (i == 1) { output.AddMember("Parameters", objectCreator(tmpVector, output), output.GetAllocator()); }
+			else if (i == 2) { output.AddMember("Variables", objectCreator(tmpVector, output), output.GetAllocator()); }
 		}
 	}
 	va_end(components);
 	return output;
-	//output.AddMember("Sets", setObject, output.GetAllocator());
-
-
 }
-void writeToFile(const char*fname, Document&output) {
+void writeToFile(const char*fname,const char*mod, Document&output) {
 	FILE* fileCreator = fopen(fname, "ab+");
 	fclose(fileCreator);
-	FILE* fp = fopen(fname, "wb"); // non-Windows use "w"
+	FILE* fp = fopen(fname, mod); // non-Windows use "w"
 	char writeBuffer[65536];
 	FileWriteStream os(fp, writeBuffer, sizeof(writeBuffer));
 	PrettyWriter<FileWriteStream> pwriter(os);
 	output.Accept(pwriter);
 	fclose(fp);
-}
-
-
-int main()
-{
-	std::vector<std::string> setst;
-	std::vector<std::string> params;
-	std::vector<std::string> vars;
-	std::map<std::string, std::string>eredmenymap;
-	Document output;
-	vars.push_back("var1");
-	vars.push_back("var2");
-	vars.push_back("x");
-	params.push_back("p1");
-	params.push_back("p2");
-	setst.push_back("Resources");
-	setst.push_back("Products");
-	output=createJSON(3,setst,params,vars);
-	writeToFile("example.json", output);
-	eredmenymap=JSON_TO_MAP("example.json", "rb");
-
-	/*for (std::map<std::string, std::string>::iterator it=eredmenymap.begin();it!=eredmenymap.end();++it)
-	{
-		std::cout << it->first << '\t' << it->second << '\n';
-	}*/
-	
 }
